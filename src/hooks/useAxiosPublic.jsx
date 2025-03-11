87fc07bef24ca6dbcf30e88f3386f50a19be5d87
ashikbillah699 <ashikbillah699@gmail.com>
2025-03-11 16:52:42 +0600

    deploy

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosPublic =  axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://bistro-boss-server-rho-ochre.vercel.app'
 })
 
 const useAxiosPublic = () => {
